1460a11fd9c9ee38d08627a514356779ce7577b4
Moreno Gentili <brightsoul@tiscali.it>
2021-02-03 13:58:05 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreIdentityMySql.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
